f58debdcb8db94cf3956b8f05ea5b7d4b01b7e33
maxazillion <47362990+maxazillion@users.noreply.github.com>
2021-03-08 18:58:24 -0500

    render images of posters and titles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,30 @@
-import { Main } from "components";
+import api from "api";
+import { Card, Main } from "components";
+import { useEffect, useState } from "react";
+
 export const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await api.index();
+      setMovies(() => results);
+    })();
+  }, []);
+
+  console.log(movies);
   return (
     <Main>
-      <p>Hello World</p>
+      {movies.map((movie, index) => {
+        return (
+          <Card
+            key={movie.id}
+            title={movie.title}
+            path={movie.poster_path}
+            id={movie.id}
+          />
+        );
+      })}
     </Main>
   );
 };
